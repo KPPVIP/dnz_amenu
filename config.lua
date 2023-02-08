f8713ae2122825e8e7cea7ca115563460fca51ce
Guudz <Kilian1702@outlook.com>
2023-02-08 06:36:12 +0100

    Initial commit

---
+++ config.lua
@@ -0,0 +1,8 @@
+Config = {}
+
+
+
+Config.ReloadBanList = "60" -- IN MINUTES
+
+Config.Screenshot = true
+Config.ScreenshotsWebhook = "https://discord.com/api/webhooks/854122262339911690/G6ZdI4FQpNtHgDHg_9GAqiT59eVZZvrmEgjt9Ti3cp8JOyVFmVzrLL7p5Uu823FWG9e9"
